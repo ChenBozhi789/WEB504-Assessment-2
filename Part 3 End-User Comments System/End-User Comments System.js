9e93d948097bc17b6abc7daf07556197da988338
ChenBozhi789 <bozhi.chen@outlook.com>
2023-11-07 23:05:46 +1300

    Last edit

--- Part 3 End-User Comments System/End-User Comments System.js
+++ Part 3 End-User Comments System/End-User Comments System.js
@@ -1,62 +1,66 @@
-// Import the firebase
-import { 
-    initializeApp 
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
-  import { 
-    getDatabase, 
-    ref, 
-    push, 
-    onValue 
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
-  
-  // Initialize Firebase application
-  const appSetting = {
-    databaseURL: 'https://web504-assessment-2-e5b0b-default-rtdb.asia-southeast1.firebasedatabase.app/',
-  };
-  
-  const app = initializeApp(appSetting);
-  const database = getDatabase(app);
-  
-  // Get page elements
-  const commentText = document.getElementById('enterSection');
-  const postCommentBtn = document.getElementById('postComment');
-  const commentsList = document.getElementById('commentsList');
-  const toggleButton = document.getElementById('toggleButton');
-  
-  postCommentBtn.addEventListener('click', () => {
-    const commentValue = commentText.value.trim();
-    if (commentValue) {
-      const commentsRef = ref(database, 'comments');
-      push(commentsRef, { text: commentValue, timestamp: Date.now() })
-        .then(() => {
-          commentText.value = ''; // Clear commentText
-          alert('Congratulations, your review has been successfully submitted!');
-        })
-        .catch((error) => {
-          console.error('Error posting comment: ', error);
-        });
-    }
+// Import Firebase applications and database modules
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
+
+// Initialize the Firebase application Settings
+const appSetting = {
+  databaseURL: 'https://web504-assessment-2-e5b0b-default-rtdb.asia-southeast1.firebasedatabase.app/',
+};
+
+// Initialize Firebase app
+const app = initializeApp(appSetting);
+
+// Get database example
+const database = getDatabase(app);
+
+// Get page element 
+const commentText = document.getElementById('enterSection'); 
+const postCommentBtn = document.getElementById('postComment'); 
+const commentsList = document.getElementById('commentsList');
+const toggleButton = document.getElementById('toggleButton'); 
+
+// Add a click event listener to the comment button
+postCommentBtn.addEventListener('click', () => {
+  const commentValue = commentText.value.trim(); // Get the comment input box
+  if (commentValue) { 
+    const commentsRef = ref(database, 'comments'); // Gets a node reference in the database
+
+    // Push the comment content and timestamp to the database
+    push(commentsRef, { text: commentValue, timestamp: Date.now() })
+      .then(() => {
+        commentText.value = ''; // Clear the comment input box
+        alert('Congratulations, your review has been successfully submitted!');
+      })
+      .catch((error) => {
+        console.error('Error Posting a comment:', error); 
+      });
+  }
+});
+
+// Real-time monitoring of comments data changes in the database
+const commentsRef = ref(database, 'comments'); // Gets a node reference in the database
+
+onValue(commentsRef, (snapshot) => {
+  const comments = snapshot.val(); // Get comment data
+
+  commentsList.innerHTML = ''; // Clear comment list
+
+  // Iterate over all the comment data and add it to the comment list for display
+  Object.values(comments).forEach((comment) => {
+    const li = document.createElement('li'); // Create a list item element
+    li.textContent = comment.text; // Set the list item text content to the comment content
+    commentsList.appendChild(li); // Adds the list item to the comment list
   });
-  
-  // Monitor changes in comment data in real time
-  const commentsRef = ref(database, 'comments');
-  onValue(commentsRef, (snapshot) => {
-    const comments = snapshot.val();
-    commentsList.innerHTML = ''; // Clear list
-  
-    Object.values(comments).forEach((comment) => {
-      const li = document.createElement('li');
-      li.textContent = comment.text;
-      commentsList.appendChild(li);
-    });
-  });
-  
-  // Expand/hide button
-  toggleButton.addEventListener('click', () => {
-    const commentSection = document.getElementById('previousComment');
-    if (!commentSection.style.display || commentSection.style.display === 'none') {
-      commentSection.style.display = 'block';
-    } else {
-      commentSection.style.display = 'none';
-    }
-  });  
+});
+
+// Add a click event listener for the expand/Hide comment list button
+toggleButton.addEventListener('click', () => {
+  const commentSection = document.getElementById('previousComment'); // Get comment part element
+
+  // Toggle the display status of the comments section
+  if (!commentSection.style.display || commentSection.style.display === 'none') {
+    commentSection.style.display = 'block'; // Displays if currently hidden
+  } else {
+    commentSection.style.display = 'none'; // If currently displayed, hide
+  }
+});
--- Part 4 User Authentication System/User Authentication System.js
+++ Part 4 User Authentication System/User Authentication System.js
@@ -1,131 +1,152 @@
-// 导入Firebase库
-import {
-    initializeApp
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
-  import {
-    getDatabase,
-    ref,
-    push,
-    onValue,
-    set
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
-  import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
+// 导入Firebase的初始化模块和数据库模块
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+import { getDatabase, ref, push, onValue, set } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
+// 导入Firebase认证模块
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
+
+// My web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9jbkSK-pwpxguJjfMgQe1-BOz8vNLcqA",
+  authDomain: "web504-assessment-2-e5b0b.firebaseapp.com",
+  databaseURL: "https://web504-assessment-2-e5b0b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web504-assessment-2-e5b0b",
+  storageBucket: "web504-assessment-2-e5b0b.appspot.com",
+  messagingSenderId: "407996235327",
+  appId: "1:407996235327:web:2899af70e826770857c28a"
+};
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Obtain the authentication service instance
+const auth = getAuth(app);
+
+// Get database example
+const database = getDatabase(app);
+
+// Get page element 
+const commentText = document.getElementById('enterSection');
+const postCommentBtn = document.getElementById('postComment');
+const commentsList = document.getElementById('commentsList'); 
+const toggleButton = document.getElementById('toggleButton'); 
+const editButton = document.getElementById('editButton'); 
+
+// User register function
+const registerUser = () => {
+  // Get user input
+  const email = document.getElementById('registerEmail').value;
+  const password = document.getElementById('registerPassword').value;
   
-  const firebaseConfig = {
-    apiKey: 'AIzaSyA9jbkSK-pwpxguJjfMgQe1-BOz8vNLcqA',
-    authDomain: 'web504-assessment-2-e5b0b.firebaseapp.com',
-    databaseURL: 'https://web504-assessment-2-e5b0b-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'web504-assessment-2-e5b0b',
-    storageBucket: 'web504-assessment-2-e5b0b.appspot.com',
-    messagingSenderId: '407996235327',
-    appId: '1:407996235327:web:2899af70e826770857c28a',
-  };
+  // Calling Firebase function to register
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => alert('Registration successful!'))
+    .catch(error => alert(`Error: ${error.message}`)); 
+};
+
+// User loginfunction
+const loginUser = () => {
+  // Get user input
+  const email = document.getElementById('loginEmail').value;
+  const password = document.getElementById('loginPassword').value;
+
+  // Calling Firebase function to login
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => alert('Login successful!'))
+    .catch(error => alert(`Error: ${error.message}`));
+};
+
+// Listen for the post comment button click event
+postCommentBtn.addEventListener('click', () => {
+  // Check if the current user is logged in
+  const user = auth.currentUser;
   
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const database = getDatabase(app);
-  
-  const commentText = document.getElementById('enterSection');
-  const postCommentBtn = document.getElementById('postComment');
-  const commentsList = document.getElementById('commentsList');
-  const toggleButton = document.getElementById('toggleButton');
-  const editButton = document.getElementById('editButton');
-  
-  const registerUser = () => {
-    const email = document.getElementById('registerEmail').value;
-    const password = document.getElementById('registerPassword').value;
-  
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => alert('Registration successful!'))
-      .catch(error => alert(`Error: ${error.message}`));
-  };
-  
-  const loginUser = () => {
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
-  
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => alert('Login successful!'))
-      .catch(error => alert(`Error: ${error.message}`));
-  };
-  
-  postCommentBtn.addEventListener('click', () => {
-    const user = auth.currentUser;
-    if (user) {
-      const commentValue = commentText.value.trim();
-      if (commentValue) {
-        const commentsRef = ref(database, 'comments');
-        push(commentsRef, {
-          text: commentValue,
-          uid: user.uid,
-          email: user.email,
+  if (user) {
+    // Get and clean the comment text
+    const commentValue = commentText.value.trim();
+
+    if (commentValue) {
+      // Get a reference to the comments in the database
+      const commentsRef = ref(database, 'comments');
+
+      // Push the comment to the database
+      push(commentsRef, {
+        text: commentValue,
+        uid: user.uid,
+        email: user.email,
+        timestamp: Date.now(),
+      })
+      .then(() => {
+        commentText.value = ''; // Clear the comment box
+        alert('Congratulations, your comment has been submitted successfully!');
+      })
+      .catch(error => console.error('Error posting comment:', error)); 
+    }
+  } else {
+    alert('Please log in first before commenting!');
+  }
+});
+
+// Listen for the toggle button click event to show or hide the comments section
+toggleButton.addEventListener('click', () => {
+  const commentSection = document.getElementById('previousComment');
+
+  // Toggle the display state of the comments section
+  commentSection.style.display = commentSection.style.display === 'none' || !commentSection.style.display
+    ? 'block'
+    : 'none';
+});
+
+// Listen for real-time changes to the comment data
+const commentsRef = ref(database, 'comments');
+onValue(commentsRef, snapshot => {
+  const comments = snapshot.val(); // Retrieve the comment data
+
+  commentsList.innerHTML = ''; // Clear the existing comments list
+
+  // Create a list item for each comment and add it to the comments list
+  Object.values(comments).forEach(comment => {
+    const li = document.createElement('li');
+    li.textContent = comment.text;
+    commentsList.appendChild(li); // Add the comment to the list
+  });
+});
+
+// Listen for the edit button click event
+editButton.addEventListener('click', () => {
+  // Prompt the user to enter the comment they want to edit
+  const userEntryUpdateDate = prompt('Please enter the content of the comment you want to edit!');
+
+  onValue(commentsRef, snapshot => {
+    const editData = snapshot.val();
+    if (!editData) {
+      alert('No data found'); // No data found
+      return;
+    }
+
+    // Find the key for the comment the user wishes to edit
+    const keyToData = Object.keys(editData).find(key => editData[key].text === userEntryUpdateDate);
+    
+    // If found, prompt the user to enter new comment content
+    if (keyToData) {
+      const newData = prompt('Please enter the new comment content');
+      if (newData) {
+        // Get a reference to the specific comment in the database
+        const specificUpdateRef = ref(database, `comments/${keyToData}`);
+        
+        // Update the comment content in the database
+        set(specificUpdateRef, {
+          text: newData,
+          uid: auth.currentUser.uid,
+          email: auth.currentUser.email,
           timestamp: Date.now(),
         })
-        .then(() => {
-          commentText.value = '';
-          alert('Congratulations, your comment has been submitted successfully!');
-        })
-        .catch(error => console.error('Error posting comment:', error));
+        .then(() => alert('Comment updated successfully!')) 
+        .catch(error => alert(`Error updating comment: ${error.message}`)); 
+      } else {
+        alert('You have canceled the operation or did not enter the edited comment, please try again');
       }
     } else {
-      alert('Please log in first before commenting!');
+      alert('Matching comment not found, please re-enter!');
     }
   });
-  
-  toggleButton.addEventListener('click', () => {
-    const commentSection = document.getElementById('previousComment');
-    commentSection.style.display = commentSection.style.display === 'none' || !commentSection.style.display
-      ? 'block'
-      : 'none';
-  });
-  
-  const commentsRef = ref(database, 'comments');
-  onValue(commentsRef, snapshot => {
-    const comments = snapshot.val();
-    commentsList.innerHTML = '';
-  
-    Object.values(comments).forEach(comment => {
-      const li = document.createElement('li');
-      li.textContent = comment.text;
-      commentsList.appendChild(li);
-    });
-  });
-  
-  editButton.addEventListener('click', () => {
-    const userEntryUpdateDate = prompt('Please enter the content of the comment you want to edit!');
-    onValue(commentsRef, snapshot => {
-      const editData = snapshot.val();
-      if (!editData) {
-        alert('No data found');
-        return;
-      }
-  
-      const keyToData = Object.keys(editData).find(key => editData[key].text === userEntryUpdateDate);
-  
-      if (keyToData) {
-        const newData = prompt('Please enter the new comment content');
-        if (newData) {
-          const specificUpdateRef = ref(database, `comments/${keyToData}`);
-          set(specificUpdateRef, {
-            text: newData,
-            uid: auth.currentUser.uid,
-            email: auth.currentUser.email,
-            timestamp: Date.now(),
-          })
-          .then(() => alert('Comment updated successfully!'))
-          .catch(error => alert(`Error updating comment: ${error.message}`));
-        } else {
-          alert('You have canceled the operation or did not enter the edited comment, please try again');
-        }
-      } else {
-        alert('Matching comment not found, please re-enter!');
-      }
-    });
-  });
-  
-  document.getElementById('registerButton').addEventListener('click', registerUser);
-  document.getElementById('loginButton').addEventListener('click', loginUser);  
+});
